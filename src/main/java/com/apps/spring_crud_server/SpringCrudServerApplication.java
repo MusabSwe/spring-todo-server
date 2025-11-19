15267c273a869f854a0599f6993323b710e74e48
musab <malqarninet@outlook.com>
2025-11-19 11:38:20 +0300

    set up todo server demo

---
+++ src/main/java/com/apps/spring_crud_server/SpringCrudServerApplication.java
@@ -0,0 +1,13 @@
+package com.apps.spring_crud_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apps/spring_crud_server/SpringCrudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apps.spring_crud_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
